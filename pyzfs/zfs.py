7496f6510b1d852a2e1dd48c4f2f81d454c9379c
Raymond Wagner <rwagner@mythtv.org>
2011-10-07 01:34:24 -0400

    Filesystem children, snapshots, and cloning
    FileSystem objects now populate a list of children as well as a parent
    attribute. FileSystem objects also populate a list of related snapshots.
    
    FileSystem objects have a new method clone(), to spawn a new snapshot if
    necessary, clone itself into a new partition, and optionally recurse
    through child partitions.

--- pyzfs/zfs.py
+++ pyzfs/zfs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from util import Popen, call
+from datetime import datetime
 
 class Property( object ):
     def __init__(self, *args):
@@ -87,8 +88,77 @@ class Properties( object ):
             prop.reset()
 
 class FileSystem( object ):
+    class _Snapshots( list ):
+        def __init__(self, parent):
+            self.parent = parent
+            super(FileSystem._Snapshots, self).__init__()
+        def populate(self, force=False):
+            if not (force or (len(self) == 0)):
+                return
+            self.clear()
+            zfs = Popen(['/sbin/zfs', 'list', '-r', '-t', 'snapshot', self.parent], stdout=-1)
+            if zfs.wait():
+                raise Exception('snapshots could not be read')
+            zfs.stdout.next()
+            for line in zfs.stdout:
+                snap = Snapshot(line.split()[0])
+                if snap.path == self.parent:
+                    self.append(snap)
+        def __repr__(self):
+            self.populate()
+            return super(FileSystem._Snapshots, self).__repr__()
+        def __getitem__(self, key):
+            self.populate()
+            return super(FileSystem._Snapshots, self).__getitem__(key)
+        def __iter__(self):
+            self.populate()
+            return super(FileSystem._Snapshots, self).__iter__()
+        def clear(self):
+            while len(self) > 0:
+                self.pop()
+
+    class _Children( list ):
+        def __init__(self, parent):
+            self.parent = parent
+            super(FileSystem._Children, self).__init__()
+        def populate(self, force=False):
+            if not (force or (len(self) == 0)):
+                return
+            self.clear()
+            zfs = Popen(['/sbin/zfs', 'list', '-r', self.parent], stdout=-1)
+            if zfs.wait():
+                raise Exception('filesystems could not be read')
+            zfs.stdout.next()
+            for line in zfs.stdout:
+                fs = FileSystem(line.split()[0])
+                if fs.parent == self.parent:
+                    self.append(fs)
+        def __repr__(self):
+            self.populate()
+            return super(FileSystem._Children, self).__repr__()
+        def __getitem__(self, key):
+            self.populate()
+            return super(FileSystem._Children, self).__getitem__(key)
+        def __iter__(self):
+            self.populate()
+            return super(FileSystem._Children, self).__iter__()
+        def clear(self):
+            while len(self) > 0:
+                self.pop()
+
+    @classmethod
+    def check(cls, name):
+        zfs = Popen(['/sbin/zfs', 'list', name], stdout=-1)
+        if zfs.wait():
+            return False
+        if zfs.stdout.next().startswith('cannot open'):
+            return False
+        return True
+
     @classmethod
     def create(cls, name, **kwargs):
+        if cls.check(name):
+            raise Exception("FileSystem already exists")
         args = []
         for k,v in kwargs:
             args += ['-o', '{0}={1}'.format(k,v)]
@@ -99,8 +169,12 @@ class FileSystem( object ):
         return '<FileSystem "%s">' % self.name
 
     def __init__(self, name):
+        if not self.check(name):
+            raise Exception("FileSystem does not exist")
         self.name = name
         self.properties = Properties(name)
+        self.snapshots = self._Snapshots(name)
+        self.children = self._Children(name)
 
     def rename(self, newname):
         call(['/sbin/zfs','rename',self.name,newname])
@@ -114,6 +188,27 @@ class FileSystem( object ):
 
     def snapshot(self, name, **kwargs):
         return Snapshot.create(self, name, **kwargs)
+
+    def clone(self, name, recursive=False, tag=None):
+        try:
+            # filesystem exists, do nothing
+            FileSystem(name)
+            return None
+        except:
+            pass
+
+        if tag is None:
+            tag = datetime.now().strftime('%Y%m%d')
+        try:
+            snap = Snapshot(self.name + '@' + tag)
+        except:
+            snap = Snapshot.create(self.name + '@' + tag)
+
+        clone = snap.clone(name)
+        if recursive:
+            for child in self.children:
+                child.clone(name + '/' + child.basename, recursive, tag)
+        return clone
 
     @property
     def used(self): return self.properties['used']
@@ -132,7 +227,25 @@ class FileSystem( object ):
     @mountpoint.setter
     def mountpoint(self, value): self.properties['mountpoint'] = value
 
+    @property
+    def basename(self): return self.name.split('/')[-1]
+
+    @property
+    def parent(self):
+        if not '/' in self.name:
+            return False
+        return self.name.rsplit('/',1)[0]
+
 class Snapshot( object ):
+    @classmethod
+    def check(cls, name):
+        zfs = Popen(['/sbin/zfs', 'list', name], stdout=-1)
+        if zfs.wait():
+            return False
+        if zfs.stdout.next().startswith('cannot open'):
+            return False
+        return True
+
     @classmethod
     def create(cls, *args, **kwargs):
         if len(args) == 1:
@@ -144,6 +257,9 @@ class Snapshot( object ):
                 parent = args[0]
             sname = "{0}@{1}".format(parent, args[1])
 
+        if cls.check(sname):
+            raise Exception("Snapshot already exists")
+
         args = []
         for k,v in kwargs:
             args += ['-o', '{0}={1}'.format(k,v)]
@@ -151,7 +267,12 @@ class Snapshot( object ):
         call(['/sbin/zfs','snapshot'] + args + [sname])
         return cls(sname)
 
+    def __repr__(self):
+        return '<Snapshot "%s">' % self.name
+
     def __init__(self, name):
+        if not self.check(name):
+            raise Exception("Snapshot does not exist")
         self.name = name
         self.properties = Properties(name)
 
@@ -160,11 +281,20 @@ class Snapshot( object ):
         for k,v in kwargs:
             args += ['-o', '{0}={1}'.format(k,v)]
         
-        call(['/sbin/zfs','clone'] + args + [name])
-        return FileSystem(sname)
+        call(['/sbin/zfs','clone'] + args + [self.name, name])
+        return FileSystem(name)
 
-    def destroy(self):
+    def destroy(self, recursive=False):
         call(['/sbin/zfs','destroy',self.name])
+        if recursive:
+            for child in FileSystem(self.path).children:
+                Snapshot(child.name + '@' + self.tag).destroy(recursive)
 
     def update(self):
         self.properties.update()
+
+    @property
+    def path(self): return self.name.split('@')[0]
+
+    @property
+    def tag(self): return self.name.split('@')[1]
